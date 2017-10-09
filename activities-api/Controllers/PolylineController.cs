db6f14dd94e255677857b04f6fa01794ce085283
Nick Wareing <nick.wareing@aranzmedical.com>
2017-10-10 09:13:25 +1300

    Initial commit

---
+++ activities-api/Controllers/PolylineController.cs
@@ -0,0 +1,25 @@
+﻿using activities_api.Services;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace activities_api.Controllers
+{
+  [EnableCors(origins: "http://localhost:9000", headers: "*", methods: "*")]
+  public class PolylineController : ApiController
+  {
+    private PolylineService polylineService;
+
+    public PolylineController()
+    {
+      this.polylineService = new PolylineService();
+    }
+
+    public IHttpActionResult Get()
+    {
+      var polylines = this.polylineService.GetPolylines().ToList();
+      return this.Ok(new { polylines = polylines });
+    }
+
+  }
+}
---
+++ activities-api/Services/PloylineService.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace activities_api.Services
+{
+  public class PolylineService
+  {
+    private SqlConnection connection;
+
+    public PolylineService()
+    {
+      this.connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Activities"].ConnectionString);
+    }
+
+    public IEnumerable<String> GetPolylines()
+    {
+      var polylines = this.connection.Query<String>(@"SELECT TOP 100 Polyline FROM dbo.Activities ORDER BY StartDateLocal DESC");
+      var escaped = polylines.Select(p => p.Replace(@"\", "\\"));
+      return escaped;
+    }
+  }
+}
